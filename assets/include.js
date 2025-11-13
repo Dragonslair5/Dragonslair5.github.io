394621ddc2816eb5d62887be944c03f49d03181f
Mauricio Palma <mauricio.gpalma@gmail.com>
2025-11-13 04:04:49 -0300

    go gogo

---
+++ assets/include.js
@@ -0,0 +1,15 @@
+async function includeHTML() {
+  const elements = document.querySelectorAll('[data-include]');
+  for (const el of elements) {
+    const file = el.getAttribute('data-include');
+    const response = await fetch(file);
+    if (response.ok) {
+      el.innerHTML = await response.text();
+    } else {
+      el.innerHTML = "Include not found.";
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", includeHTML);
+
